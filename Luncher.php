916868de437553396020980c8b9ce2a79a98e627
rezarph <xse1@yahoo.com>
2016-08-03 19:15:14 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '251531797:AAFXlERtAwxZOkU-e7Rn1GinUltuuMONIjg');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
